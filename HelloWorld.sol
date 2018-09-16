8ee068fac0eb15d3d0735a6911cf13e2dff1baa5
Nishant Kumar Jaiswal <42875579+nishantworkspace@users.noreply.github.com>
2018-09-16 21:20:45 +0530

    Add files via upload

---
+++ HelloWorld.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.0;
+
+contract HelloWorld {
+    string  word = "Hello World";
+    address issuer; 
+    function HelloWorld()
+    {
+            issuer = msg.sender;
+            }
+    
+    
+    function getword() constant returns(string)
+    {
+        return word;
+    }
+    function setword (string newword) returns(string)
+    {
+       if (issuer != msg.sender)
+       {
+           word = "Not a owner";
+           return word;
+       }
+        word = newword;
+        return word;
+
+    }
+    
+    
+}
